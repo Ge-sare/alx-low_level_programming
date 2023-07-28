ca5c27ad3f9e90f9b30be3635c298ac8e5e004d5
Ge-sare <okemwafg@gmail.com>
2023-07-28 07:45:09 -0700

    commit to correct 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,17 +11,16 @@ void print_most-numbers(void)
 {
 	int x = 0;
 
-	for (; x <= '9'; x++)
+	for (; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
+		if (x ==2 || x ==4)
 		{
 			continue;
 		}
 		else
 		{
-			_putchar(x + '8');
+			_putchar(x + '0');
 		}
 	}
-
 	_putchar('\n')
 }
