857cc571b4451a7f5660e02a76182edb1b883771
Ge-sare <okemwafg@gmail.com>
2023-10-09 05:11:54 -0700

    commit to 101-keygen.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,20 +3,20 @@
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
- * Return: the int conveted from the string
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	i = 0
-	d = 0
-	n = 0
-	len = 0
-	f = 0
-	digit = 0
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	while (s[len] != '\0'
+	while (s[len] != '\0')
 		len++;
 
 	while (i < len && f == 0)
@@ -33,7 +33,7 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9'
 					break;
-			f = 0
+			f = 0;
 		}
 		i++;
 	}
